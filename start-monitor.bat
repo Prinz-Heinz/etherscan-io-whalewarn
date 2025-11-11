90225740a8e7360c46a872e1bf2b37448025f31e
Prinz-Heinz <prinzheinzketchup@gmail.com>
2025-11-12 02:04:25 +0800

    Modified start-monitor.bat to properly use change directory command to current working directory.

--- start-monitor.bat
+++ start-monitor.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
-rem Run monitor.py again in 15 seconds if connection lost or script dies
 
+rem Run monitor.py again in 15 seconds if connection lost or script dies
 :loop
 
 python monitor.py
@@ -9,3 +9,5 @@ python monitor.py
 ping localhost -n 15 > nul
 
 goto :loop
+
+exit
